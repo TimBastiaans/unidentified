4b7dae65370b96b83b6a11082197ad4e62565f2c
nymtesx <22098595+nymtesx@users.noreply.github.com>
2023-03-21 21:02:01 +0100

    tutorial initial push

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config({path: 'backend/.env'})
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/api/workouts', workoutRoutes )
+
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
